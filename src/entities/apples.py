28dd33f301fdadc36f816d147982a8f7dee9bf13
Thomas Grundstrom <thomas.grundstrom@helsinki.fi>
2023-04-03 12:46:06 +0300

    apples.py created

---
+++ src/entities/apples.py
@@ -0,0 +1,11 @@
+import random
+
+class Apple:
+    def __init__(self):
+        self.coordinates = [0, 0]
+    
+    def spawn(self):
+        y = random.randint(1, 15)
+        x = random.randint(1, 15)
+        self.coordinates[0] = y
+        self.coordinates[1] = x
