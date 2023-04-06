e6113f6560dc8bb54b5d7a9099a57cad69c3422b
Thomas Grundstrom <thomas.grundstrom@helsinki.fi>
2023-04-06 18:30:35 +0300

    snake test file update

--- src/tests/snake_test.py
+++ src/tests/snake_test.py
@@ -7,8 +7,7 @@ class TestSnake(unittest.TestCase):
         self.snake = Snake()
 
     def test_snake_starting_values(self):
-        self.assertEqual(self.snake.length, 2)
-        self.assertEqual(self.snake.x, 2)
+        self.assertEqual(self.snake.x, 4)
         self.assertEqual(self.snake.y, 4)
         self.assertEqual(self.snake.up, False)
         self.assertEqual(self.snake.down, False)
@@ -41,20 +40,20 @@ class TestSnake(unittest.TestCase):
         self.assertEqual(self.snake.right, True)
         self.assertEqual(self.snake.down, False)
 
-    def test_adding_length_works(self):
-        snake = Snake()
-        snake.add_length()
-        self.assertEqual(snake.length, 3)
-
     def test_moving_snake_works(self):
         snake = Snake()
         self.assertEqual(snake.y, 4)
-        self.assertEqual(snake.x, 2)
+        self.assertEqual(snake.x, 4)
         self.assertEqual(snake.right, True)
         snake.move_snake()
         self.assertEqual(snake.y, 4)
-        self.assertEqual(snake.x, 3)
+        self.assertEqual(snake.x, 5)
         snake.moveup()
         snake.move_snake()
         self.assertEqual(snake.y, 3)
-        self.assertEqual(snake.x, 3)
+        self.assertEqual(snake.x, 5)
+    
+    def test_snake_body_moves_correctly(self):
+        self.assertEqual(self.snake.positions, [(4, 3), (4, 4)])
+        self.snake.move_snake()
+        self.assertEqual(self.snake.positions, [(4, 4), (4, 5)])
