d0876624566b29cd021bfa8505279427925ab72b
Thomas Grundstrom <thomas.grundstrom@helsinki.fi>
2023-04-06 18:30:14 +0300

    snakemap file update

--- src/snakemap/snakemap.py
+++ src/snakemap/snakemap.py
@@ -2,21 +2,22 @@ from entities.snake import snake
 
 class Gamemap:
     def __init__(self):
-        self.map = [[0 for i in range(10)] for j in range(10)]
+        self.grid = [[0 for i in range(10)] for j in range(10)]
         for i in range(10):
-            self.map[i][0] = 1
-            self.map[i][9] = 1
-            self.map[0][i] = 1
-            self.map[9][i] = 1
-            for j in range(10):
-                if i == snake.y and j == snake.x:
-                    self.map[i][j] = 2
+            self.grid[i][0] = 1
+            self.grid[i][9] = 1
+            self.grid[0][i] = 1
+            self.grid[9][i] = 1
+        self.grid[4][4] = 2
+        self.grid[4][3] = 2
     
     def update(self):
         for y in range(10):
             for x in range(10):
-                if y == snake.y and x == snake.x:
-                    self.map[y][x] = 2
-        return self.map
+                if (y, x) in snake.positions:
+                    self.grid[y][x] = 2
+                elif (y, x) not in snake.positions and self.grid[y][x] != 1:
+                    self.grid[y][x] = 0
+        return self.grid
 
 snakemap = Gamemap()
