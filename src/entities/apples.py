7eee4c8078615d09ab8b23db81b0d3cf64f8a080
Thomas Grundstrom <thomas.grundstrom@helsinki.fi>
2023-04-06 18:27:55 +0300

    small apples file update

--- src/entities/apples.py
+++ src/entities/apples.py
@@ -3,10 +3,11 @@ import random
 
 class Apple:
     def __init__(self):
-        self.coordinates = [0, 0]
+        self.y = 7
+        self.x = 7
 
     def spawn(self):
-        y = random.randint(1, 15)
-        x = random.randint(1, 15)
-        self.coordinates[0] = y
-        self.coordinates[1] = x
+        y = random.randint(1, 8)
+        x = random.randint(1, 8)
+        self.y = y
+        self.x = x
