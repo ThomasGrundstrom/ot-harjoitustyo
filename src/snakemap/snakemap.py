107ba689f0573266019df8706d91a77e99a06685
Thomas Grundstrom <thomas.grundstrom@helsinki.fi>
2023-04-07 18:32:38 +0300

    updated how map works

--- src/snakemap/snakemap.py
+++ src/snakemap/snakemap.py
@@ -1,4 +1,6 @@
 from entities.snake import snake
+from actions.actions import actions
+from entities.apples import apple
 
 
 class Gamemap:
@@ -11,14 +13,19 @@ class Gamemap:
             self.grid[11][i] = 1
         self.grid[4][4] = 2
         self.grid[4][3] = 2
+        self.grid[7][7] = 3
 
     def update(self):
+        actions.check_actions()
+        snake.move_snake()
         for y in range(12):
             for x in range(12):
                 if (y, x) in snake.positions:
                     self.grid[y][x] = 2
                 elif (y, x) not in snake.positions and self.grid[y][x] != 1:
                     self.grid[y][x] = 0
+                if y == apple.y and x == apple.x and not (apple.y, apple.x) in snake.positions:
+                    self.grid[y][x] = 3
         return self.grid
 
 
