0d26b90e94dddd0daa89a70820a7776d05c399bb
Thomas Grundstrom <thomas.grundstrom@helsinki.fi>
2023-04-07 18:32:15 +0300

    screen class updates

--- src/screen/loadgame.py
+++ src/screen/loadgame.py
@@ -1,6 +1,4 @@
 import pygame
-from actions.actions import actions
-from entities.snake import snake
 from snakemap.snakemap import snakemap
 
 
@@ -17,6 +15,7 @@ class Screen:
         black = (0, 0, 0)
         white = (255, 255, 255)
         green = (0, 255, 0)
+        red = (255, 0, 0)
         self.screen.fill(white)
         for y in range(12):
             for x in range(12):
@@ -24,14 +23,14 @@ class Screen:
                     pygame.draw.rect(self.screen, black, (y*68, x*68, 68, 68))
                 elif snakemap.grid[y][x] == 2:
                     pygame.draw.rect(self.screen, green, (y*68, x*68, 68, 68))
+                elif snakemap.grid[y][x] == 3:
+                    pygame.draw.rect(self.screen, red, (y*68, x*68, 68, 68))
 
         pygame.display.flip()
-        self.clock.tick(2)
+        self.clock.tick(3)
 
     def loop(self):
         while True:
-            actions.check_actions()
-            snake.move_snake()
             snakemap.update()
             self.draw_screen()
 
