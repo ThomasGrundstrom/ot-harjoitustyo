756e2c383b57dccdbc6761e7e127dc836dea7c9d
Thomas Grundstrom <thomas.grundstrom@helsinki.fi>
2023-04-28 18:42:00 +0300

    optimized snake movement code

--- src/entities/snake.py
+++ src/entities/snake.py
@@ -5,56 +5,50 @@ class Snake:
     def __init__(self):
         self.x = 4
         self.y = 4
-        self.up = False
-        self.down = False
-        self.left = False
-        self.right = True
+        self.direction = 1
+        self.moving = True
         self.changeddirections = False
         self.positions = [(4, 3), (4, 4)]
 
     def moveup(self):
-        if (self.left or self.right) and not self.changeddirections:
-            self.right = False
-            self.left = False
-            self.up = True
+        if (self.direction == 1 or self.direction == 3) and not self.changeddirections:
+            self.direction = 0
             self.changeddirections = True
 
     def movedown(self):
-        if (self.left or self.right) and not self.changeddirections:
-            self.right = False
-            self.left = False
-            self.down = True
+        if (self.direction == 1 or self.direction == 3) and not self.changeddirections:
+            self.direction = 2
             self.changeddirections = True
 
     def moveleft(self):
-        if (self.up or self.down) and not self.changeddirections:
-            self.up = False
-            self.down = False
-            self.left = True
+        if (self.direction == 0 or self.direction == 2) and not self.changeddirections:
+            self.direction = 3
             self.changeddirections = True
 
     def moveright(self):
-        if (self.up or self.down) and not self.changeddirections:
-            self.up = False
-            self.down = False
-            self.right = True
+        if (self.direction == 0 or self.direction == 2) and not self.changeddirections:
+            self.direction = 1
             self.changeddirections = True
 
     def move_snake(self):
-        if self.up:
-            self.y -= 1
-        elif self.down:
-            self.y += 1
-        elif self.left:
-            self.x -= 1
-        elif self.right:
-            self.x += 1
-        if self.positions[-1] != (apple.y, apple.x):
-            self.positions.pop(0)
-        elif self.positions[-1] == (apple.y, apple.x):
-            apple.spawn()
-        self.positions.append((self.x, self.y))
-        self.changeddirections = False
+        if self.moving:
+            if self.direction == 0:
+                self.y -= 1
+            elif self.direction == 2:
+                self.y += 1
+            elif self.direction == 3:
+                self.x -= 1
+            elif self.direction == 1:
+                self.x += 1
+            if self.positions[-1] != (apple.y, apple.x):
+                self.positions.pop(0)
+            elif self.positions[-1] == (apple.y, apple.x):
+                apple.spawn()
+            self.positions.append((self.x, self.y))
+            self.changeddirections = False
+    
+    def set_stationary(self):
+        self.moving = False
 
 
 snake = Snake()
