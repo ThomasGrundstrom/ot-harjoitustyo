c77e6778501245aba97b300750c9fa0da5c24bfb
Thomas Grundstrom <thomas.grundstrom@helsinki.fi>
2023-05-14 12:52:27 +0300

    autopep8 formatting

--- src/tests/screen_test.py
+++ src/tests/screen_test.py
@@ -2,10 +2,11 @@ import unittest
 import pygame
 from screen.loadgame import Screen
 
+
 class TestScreen(unittest.TestCase):
     def setUp(self):
         self.screen = Screen()
-    
+
     def test_screen_initialization(self):
         self.assertEqual(self.screen.screen.get_width(), 816)
         self.assertEqual(self.screen.screen.get_height(), 816)
@@ -20,31 +21,51 @@ class TestScreen(unittest.TestCase):
     def test_screen_color_wingame(self):
         self.screen.wingame()
         self.assertEqual(self.screen.screen.get_at((0, 0)), self.screen.white)
-        self.assertEqual(self.screen.screen.get_at((0, 815)), self.screen.white)
-        self.assertEqual(self.screen.screen.get_at((815, 0)), self.screen.white)
-        self.assertEqual(self.screen.screen.get_at((815, 815)), self.screen.white)
-        
+        self.assertEqual(self.screen.screen.get_at(
+            (0, 815)), self.screen.white)
+        self.assertEqual(self.screen.screen.get_at(
+            (815, 0)), self.screen.white)
+        self.assertEqual(self.screen.screen.get_at(
+            (815, 815)), self.screen.white)
+
     def test_screen_color_losegame(self):
         self.screen.losegame()
         self.assertEqual(self.screen.screen.get_at((0, 0)), self.screen.black)
-        self.assertEqual(self.screen.screen.get_at((0, 815)), self.screen.black)
-        self.assertEqual(self.screen.screen.get_at((815, 0)), self.screen.black)
-        self.assertEqual(self.screen.screen.get_at((815, 815)), self.screen.black)
-    
+        self.assertEqual(self.screen.screen.get_at(
+            (0, 815)), self.screen.black)
+        self.assertEqual(self.screen.screen.get_at(
+            (815, 0)), self.screen.black)
+        self.assertEqual(self.screen.screen.get_at(
+            (815, 815)), self.screen.black)
+
     def test_startmenu(self):
         self.screen.draw_screen()
         self.assertEqual(self.screen.screen.get_at((0, 0)), self.screen.white)
-        self.assertEqual(self.screen.screen.get_at((0, 815)), self.screen.white)
-        self.assertEqual(self.screen.screen.get_at((815, 0)), self.screen.white)
-        self.assertEqual(self.screen.screen.get_at((815, 815)), self.screen.white)
-        self.assertEqual(self.screen.screen.get_at((340, 300)), self.screen.green)
-        self.assertEqual(self.screen.screen.get_at((700, 250)), self.screen.green)
-        self.assertEqual(self.screen.screen.get_at((650, 100)), self.screen.green)
-        self.assertEqual(self.screen.screen.get_at((450, 100)), self.screen.green)
-        self.assertEqual(self.screen.screen.get_at((200, 50)), self.screen.green)
-        self.assertEqual(self.screen.screen.get_at((150, 50)), self.screen.green)
-        self.assertEqual(self.screen.screen.get_at((100, 150)), self.screen.green)
-        self.assertEqual(self.screen.screen.get_at((150, 350)), self.screen.green)
-        self.assertEqual(self.screen.screen.get_at((150, 500)), self.screen.green)
-        self.assertEqual(self.screen.screen.get_at((300, 450)), self.screen.green)
-        self.assertEqual(self.screen.screen.get_at((500, 450)), self.screen.red)
+        self.assertEqual(self.screen.screen.get_at(
+            (0, 815)), self.screen.white)
+        self.assertEqual(self.screen.screen.get_at(
+            (815, 0)), self.screen.white)
+        self.assertEqual(self.screen.screen.get_at(
+            (815, 815)), self.screen.white)
+        self.assertEqual(self.screen.screen.get_at(
+            (340, 300)), self.screen.green)
+        self.assertEqual(self.screen.screen.get_at(
+            (700, 250)), self.screen.green)
+        self.assertEqual(self.screen.screen.get_at(
+            (650, 100)), self.screen.green)
+        self.assertEqual(self.screen.screen.get_at(
+            (450, 100)), self.screen.green)
+        self.assertEqual(self.screen.screen.get_at(
+            (200, 50)), self.screen.green)
+        self.assertEqual(self.screen.screen.get_at(
+            (150, 50)), self.screen.green)
+        self.assertEqual(self.screen.screen.get_at(
+            (100, 150)), self.screen.green)
+        self.assertEqual(self.screen.screen.get_at(
+            (150, 350)), self.screen.green)
+        self.assertEqual(self.screen.screen.get_at(
+            (150, 500)), self.screen.green)
+        self.assertEqual(self.screen.screen.get_at(
+            (300, 450)), self.screen.green)
+        self.assertEqual(self.screen.screen.get_at(
+            (500, 450)), self.screen.red)
