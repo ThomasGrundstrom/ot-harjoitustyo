b37640631d77186f728ba2dde16390be6a9ac2eb
Thomas Grundstrom <thomas.grundstrom@helsinki.fi>
2023-05-14 12:45:48 +0300

    updated snake class test file

--- src/tests/snake_test.py
+++ src/tests/snake_test.py
@@ -11,6 +11,8 @@ class TestSnake(unittest.TestCase):
         self.assertEqual(self.snake.y, 4)
         self.assertEqual(self.snake.direction, 1)
         self.assertEqual(self.snake.moving, False)
+        self.assertEqual(self.snake.changeddirections, False)
+        self.assertEqual(self.snake.positions, [(3, 4), (4, 4)])
 
     def test_snake_turns_correctly(self):
         self.snake.moveleft()
@@ -38,22 +40,28 @@ class TestSnake(unittest.TestCase):
         self.assertEqual(self.snake.direction, 1)
 
     def test_moving_snake_works(self):
-        snake = Snake()
-        self.assertEqual(snake.y, 4)
-        self.assertEqual(snake.x, 4)
-        self.assertEqual(snake.direction, 1)
-        snake.moveright()
-        self.assertEqual(snake.moving, True)
-        snake.move_snake()
-        self.assertEqual(snake.y, 4)
-        self.assertEqual(snake.x, 5)
-        snake.moveup()
-        snake.move_snake()
-        self.assertEqual(snake.y, 3)
-        self.assertEqual(snake.x, 5)
+        self.assertEqual(self.snake.moving, False)
+        self.assertEqual(self.snake.changeddirections, False)
+        self.snake.moveright()
+        self.assertEqual(self.snake.moving, True)
+        self.snake.move_snake()
+        self.assertEqual(self.snake.changeddirections, False)
+        self.assertEqual(self.snake.y, 4)
+        self.assertEqual(self.snake.x, 5)
+        self.snake.moveup()
+        self.snake.move_snake()
+        self.assertEqual(self.snake.y, 3)
+        self.assertEqual(self.snake.x, 5)
 
     def test_snake_body_moves_correctly(self):
         self.assertEqual(self.snake.positions, [(3, 4), (4, 4)])
         self.snake.moveright()
         self.snake.move_snake()
         self.assertEqual(self.snake.positions, [(4, 4), (5, 4)])
+
+    def test_snake_stops_moving(self):
+        self.assertEqual(self.snake.moving, False)
+        self.snake.moveright()
+        self.assertEqual(self.snake.moving, True)
+        self.snake.set_stationary()
+        self.assertEqual(self.snake.moving, False)
