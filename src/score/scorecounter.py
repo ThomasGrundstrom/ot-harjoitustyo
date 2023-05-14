e877e52ce0871eb134c77a1d3933c694c6a77bfa
Thomas Grundstrom <thomas.grundstrom@helsinki.fi>
2023-05-14 14:15:02 +0300

    added high score counter

--- src/score/scorecounter.py
+++ src/score/scorecounter.py
@@ -4,15 +4,18 @@ class Scorecounter:
 
     def __init__(self):
 
-        # Constructor that initially sets the score to 0.
+        # Constructor that initially sets the score and high score to 0.
 
         self.score = 0
+        self.highscore = 0
 
     def add_score(self):
 
-        # Adds 1 to the score.
+        # Adds 1 to the score. Changes the high score when necessary.
 
         self.score += 1
+        if self.score > self.highscore:
+            self.highscore = self.score
 
 
 scorecounter = Scorecounter()
