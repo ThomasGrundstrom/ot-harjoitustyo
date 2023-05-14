7a8e646de674a8781ff9dc239cfef3d1f9f046d4
Thomas Grundstrom <thomas.grundstrom@helsinki.fi>
2023-05-14 15:49:33 +0300

    added tests for scorecounter

---
+++ src/tests/scorecounter_test.py
@@ -0,0 +1,19 @@
+import unittest
+from score.scorecounter import Scorecounter
+
+class TestScorecounter(unittest.TestCase):
+    def setUp(self):
+        self.scorecounter = Scorecounter()
+    
+    def test_starting_values(self):
+        self.assertEqual(self.scorecounter.score, 0)
+        self.assertEqual(self.scorecounter.highscore, 0)
+    
+    def test_score_gets_added(self):
+        self.scorecounter.add_score()
+        self.assertEqual(self.scorecounter.score, 1)
+        self.assertEqual(self.scorecounter.highscore, 1)
+        self.scorecounter.highscore = 50
+        self.scorecounter.add_score()
+        self.assertEqual(self.scorecounter.score, 2)
+        self.assertEqual(self.scorecounter.highscore, 50)
